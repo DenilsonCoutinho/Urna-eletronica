0d9f653e3a944f51cb52b4e2d23df1b5e7acd84c
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2025-04-11 15:05:50 -0300

    ajustando url

--- hooks/fetchVote.ts
+++ hooks/fetchVote.ts
@@ -1,10 +1,1 @@
-export async function fetchVotes() {
-    const baseUrl = process.env.NEXT_PUBLIC_URL || "http://localhost:3000"
-    const res = await fetch(`${baseUrl}/api/votes`, {
-        next: { revalidate: 600 } // 10 minutos
-    }
-    )
-    const data = await res.json()
-    return data
-}
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 
 import Link from "next/link";
-import { fetchVotes } from "../../hooks/fetchVote";
-
 
 export default async function Home() {
-  const votes = await fetchVotes() || []
+  const baseUrl = process.env.NEXT_PUBLIC_URL || "http://localhost:3000"
+  const res = await fetch(`${baseUrl}/api/votes`, {
+      next: { revalidate: 600 } // 10 minutos
+  }
+  )
+  const votes = await res.json()
+ 
 
   return (
     <main className="flex flex-col">
