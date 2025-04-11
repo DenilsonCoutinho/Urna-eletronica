571e48f15071f5441b0f098bfcf9191060da2853
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2025-04-11 14:01:50 -0300

    ajustando bug

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -4,6 +4,8 @@ import { db as prisma } from '@/lib/db'
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
     const email = searchParams.get('email')
+
+      if(!email) return Response.json([])
     const pipeline: any[] = [
         { $match: { email: email } },
         { $unwind: "$voteIn" },
